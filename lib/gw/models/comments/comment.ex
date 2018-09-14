adc5a42a1efd7c52cb3bf07450106c50bf968e58
Blake <blake@sightsource.net>
2018-09-14 17:13:11 -0400

    middle of image_lease model

--- lib/gw/models/comments/comment.ex
+++ lib/gw/models/comments/comment.ex
@@ -1,25 +1,35 @@
 defmodule GW.Models.Comments.Comment do
     use Ecto.Schema
     import Ecto.Changeset
-    alias GW.Models.Comment.Comment
+    alias GW.Models.{
+    Accounts.User,
+    Comments.CommentType,
+    Requests.RequestTrailerService,
+    Requests.RequestTrailerInspection,
+    Requests.RequestTrailerMaintenance,
+    History.HistoryTrailerInspection,
+    Routes.Route,
+    Routes.RouteSegment,
+    Trailers.TrailerLeaseAgreement}
+
 
     schema "comment" do
         field :comment_text, :string
 
-        belongs_to :comment_type, GW.Models.Comments.CommentType
-        belongs_to :user, GW.Models.Accounts.User
+        belongs_to :comment_type, CommentType
+        belongs_to :user, User
 
-        has_many :request_trailer_serivce, GW.Models.Requests.RequestTrailerService, [foreign_key: :request_comment_id]
-        has_many :request_trailer_serivce, GW.Models.Requests.RequestTrailerService, [foreign_key: :trailer_condition_comment_id]
-        has_many :request_trailer_serivce, GW.Models.Requests.RequestTrailerService, [foreign_key: :comment_large_item_id]
-        has_many :request_trailer_inspection, GW.Models.Requests.RequestTrailerInspection, [foreign_key: :inspection_comment_id]
-        has_many :request_unintended_donation, GW.Models.Requests.RequestUnintendedDonation, [foreign_key: :dsc]
-        has_many :history_trailer_inspection, GW.Models.History.HistoryTrailerInspection, [foreign_key: :inspection_comment_id]
-        has_many :route, GW.Models.Routes.Route, [foreign_key: :route_comment_id]
-        has_many :route_segment, GW.Models.Routes.RouteSegment, [foreign_key: :route_segment_comment_id]
-        has_many :trailer_lease_agreement, GW.Models.Trailers.TrailerLeaseAgreement, [foreign_key: :lease_trailer_condition_comment_id]
-        has_many :request_trailer_maintenance, GW.Model.Requests.RequestTrailerMaintenance, [foreign_key: :trailer_condition_comment_id]
-        has_many :request_trailer_maintenance, GW.Model.Requests.RequestTrailerMaintenance, [foreign_key: :maintenance_comment_id]
+        has_many :request_trailer_serivce, RequestTrailerService, [foreign_key: :request_comment_id]
+        has_many :request_trailer_serivce, RequestTrailerService, [foreign_key: :trailer_condition_comment_id]
+        has_many :request_trailer_serivce, RequestTrailerService, [foreign_key: :comment_large_item_id]
+        has_many :request_trailer_inspection, RequestTrailerInspection, [foreign_key: :inspection_comment_id]
+        has_many :request_unintended_donation, RequestUnintendedDonation, [foreign_key: :dsc]
+        has_many :history_trailer_inspection, HistoryTrailerInspection, [foreign_key: :inspection_comment_id]
+        has_many :route, Route, [foreign_key: :route_comment_id]
+        has_many :route_segment, RouteSegment, [foreign_key: :route_segment_comment_id]
+        has_many :trailer_lease_agreement, TrailerLeaseAgreement, [foreign_key: :lease_trailer_condition_comment_id]
+        has_many :request_trailer_maintenance, RequestTrailerMaintenance, [foreign_key: :trailer_condition_comment_id]
+        has_many :request_trailer_maintenance, RequestTrailerMaintenance, [foreign_key: :maintenance_comment_id]
 
         timestamps(type: :utc_datetime)
     end
